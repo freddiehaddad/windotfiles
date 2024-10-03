a872dab012d6d0c6293482a8731d5991d9f78f98
Freddie Haddad <freddie.haddad@outlook.com>
2024-10-03 15:54:57 -0700

    Remove backup.ps1 script

--- backup.ps1
+++
@@ -1,4 +0,0 @@
-# Backup Windows program settings
-$argumentList = "$env:USERPROFILE\.config\", ".\.config", "/mir", "/copyall"
-& "robocopy" $argumentList
-# Start-Process -FilePath "$copyProgram" -ArgumentList "$rootDirectory\.config\", ".\.config", "/mir", "/copyall" -NoNewWindow
