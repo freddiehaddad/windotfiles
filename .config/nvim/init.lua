ee320f134fd689302413b151d969edd0633496c3
Freddie Haddad <freddie.haddad@outlook.com>
2026-01-10 17:49:35 -0800

    visual fix + windows shell setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,24 +5,40 @@ vim.cmd.colorscheme("peanut-dark")
 vim.opt.mouse = ""
 vim.opt.clipboard = "unnamedplus"
 
-vim.opt.number = true
-vim.opt.relativenumber = true
 vim.opt.wrap = false
-
 vim.opt.conceallevel = 2
 
-vim.opt.foldcolumn = "1"
-vim.opt.foldlevel = 99
-
-vim.opt.cursorline = true
+vim.opt.pumheight = 20
 
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
-vim.opt.pumheight = 20
+vim.opt.cursorline = true
 
+vim.opt.foldcolumn = "1"
+vim.opt.foldlevel = 99
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.statuscolumn = "%C %s%=%{v:relnum==0 ? v:lnum : v:relnum} "
+
+-- Windows shell setup
+if vim.fn.has("win32") == 1 then
+	vim.opt.shell = "pwsh.exe"
+	vim.opt.shellcmdflag = "-NoLogo -ExecutionPolicy RemoteSigned -Command"
+	-- tee is a GNU utility, on windows output to a file
+	vim.opt.shellpipe = ">%s 2>&1"
+	vim.opt.shellredir = ">%s 2>&1"
+	-- For handling spaces in Windows paths correctly
+	vim.opt.shellquote = ""
+	vim.opt.shellxquote = ""
+end
+
+vim.opt.grepprg = "rg --vimgrep --smart-case --follow"
+
+-- lsp
 vim.lsp.enable({ "clangd", "luals", "rust_analyzer" })
-
 require("blink.cmp").setup({
 	keymap = {
 		["<C-space>"] = false,
